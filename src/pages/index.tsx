65a73f7c9315b7f46dc62bc01e5b1bbadeae82f5
Philip <philip@northarrowresearch.com>
2026-03-31 17:28:52 -0700

    product descriptions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,42 +10,42 @@ const tools = [
     image: '/img/products/data_exchange.png',
     href: 'https://data.riverscapes.net/',
     description:
-      'Store, curate, share and discover geospatial data related to rivers with browser-based map visualization.',
+      'Store, curate, share and discover geospatial data related to rivers. The exchange incorporates our WebViewer interactive web mapping interface for viewing data without the need for desktop GIS software. A minimal metadata standard is all that is required to make data compatible with the warehouse.',
   },
   {
     name: 'Cyber Castor',
     image: '/img/products/cybercastor.png',
     href: 'https://docs.riverscapes.net/products/cyber-castor',
     description:
-      'Provision and manage millions of parallel model runs with robust cloud queuing and execution.',
+      'Provision and manage millions of parallel  model runs. Cyber Castor handles job queuing, log file management and execution all cost-effectively and reliably performed in the cloud. Integrates with the Riverscapes Data Exchange to leverage existing data, or use APIs with external systems.',
   },
   {
     name: 'Riverscapes Reports',
     image: '/img/products/reports.png',
-    href: 'https://phlux.riverscapes.net/',
+    href: 'https://reports.riverscapes.net/',
     description:
-      'Generate professional reporting for river health metrics across large watersheds and regions.',
+      'Petabyte scale reporting of river health for tens of millions of riverscapes across North America and New Zealand. Draw or upload areas of interest to generate professional, curated reports with graphics and tables summarizing hundreds of available river metrics.',
   },
   {
     name: 'Cross Section Viewer',
     image: '/img/products/cross-section-viewer.png',
     href: 'https://xsviewer.northarrowresearch.com/',
     description:
-      'Manage and visualize river transect data with profile, areal and longitudinal tools.',
+      'Manage and visualize river transect data using a simple data model that accommodates most types of survey data. Interactive tools include areal, profile and longitudinal plots as well as several geomorphic calculations that would typically take a user a considerable amount of time to produce.',
   },
   {
     name: 'INSTAR',
     image: '/img/products/instar.png',
     href: 'http://instar.namc-usu.org/',
     description:
-      'Cloud-based geospatial database and lab software for taxonomic macroinvertebrate workflows.',
+      'Geospatial database and laboratory software for tracking taxonomic identification of macro invertebrate organisms. Cloud-based, INSTAR provides a secure GraphQL API and R Package for data retrieval and analysis.',
   },
   {
     name: 'GCD',
     image: '/img/products/gcd.png',
     href: 'http://gcd.riverscapes.net/',
     description:
-      'Quantify raster change while accounting for uncertainty using ArcGIS AddIn or standalone software.',
+      'The Geomorphic Change Detection (GCD) is an ArcGIS AddIn or standalone software for quantifying the difference between raster datasets while taking uncertainty into account. Originally designed for differences in Digital Elevation Models (DEM) the GCD can measure change between any two raster datasets.',
   },
 ];
 
