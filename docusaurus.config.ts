d4112977b6156db76c97e6c4e0d9ac486f28deda
Matt Reimer <matt@northarrowresearch.com>
2026-04-02 15:40:18 -0700

    change baseurl for test 2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,12 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://northarrowresearch.com',
+  // url: 'https://northarrowresearch.com',
+  url: 'https://northarrowresearch.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: '/nar-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
